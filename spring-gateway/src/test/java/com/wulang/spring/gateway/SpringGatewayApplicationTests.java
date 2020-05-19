862b07275fc8bce24208548eb04da13c3987dead
wulang <1359208000@qq.com>
2020-05-19 19:40:23 +0800

    gateway

---
+++ spring-gateway/src/test/java/com/wulang/spring/gateway/SpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wulang.spring.gateway;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
